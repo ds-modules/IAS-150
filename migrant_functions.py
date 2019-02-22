8c1c9cfd7c4576b8d1fe3fccfa2e8caacfbcefc3
Alex A <mondal.alex@gmail.com>
2019-02-22 00:34:41 -0800

    added functionality to select and clean relevant data

---
+++ migrant_functions.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+# select certain countries from table, either from origin or destination
+def load_data_excel(filename):
+    table = pd.read_excel(filename)
+    table = table.loc[table['Code.1'] <900,:]
+    table = table.drop(columns=['Code.1', 'Other North', 'Other South'])
+    table = table.replace('..', 0)
+    return table
+
+# select a region of countries as specified in the spec
+def select_region(table, region_list, origin=False):
+    if origin:
+        return table.loc[:,['Year', 'Code', 'Country', 'Total'] + region_list]
+    else:
+        return table.loc[table['Country'].isin(region_list), :]
+
+# get the top five origin/destination countries for a given country
+def get_top_i(table, country, year, origin=False, i=5):
+    if origin:
+        table = table.loc[table['Year'] == year, :]
+        table = table.loc[:,['Year', 'Code', 'Country', country]]
+        table = table.sort_values(by=[country], ascending=False)[0:i]
+        table = table.rename(columns={'Country': 'Country of Destination', country: 'Country of Origin: ' + country + ' (Counts)'})
+    else:
+        table = table.loc[table['Year'] == year, :]
+        table = table.loc[table['Country'] == country,:]
+        table = columns_to_rows(table)
+        table = table.sort_values(by=['Country of Destination: ' + country + ' (Counts)'], ascending=False)[0:i]
+    return table
+
+
+# helper function to re-format table in case of tracking in-migration
+def columns_to_rows(table):
+
+    country_original = table['Country'].iloc[0]
+    country_list = list(table)[4:]
+    year = table.iloc[0,0]
+    code = table.iloc[0,1]
+    first_country = country_list[0]
+
+    new_df = pd.DataFrame({'Year': [year],'Code': [code],'Country of Origin': [first_country], 'Country of Destination: '+ country_original + ' (Counts)': table[first_country].iloc[0]})
+
+    for country in country_list[1:]:
+        new_df = new_df.append({'Year': year,'Code': code,'Country of Origin': country, 'Country of Destination: '+ country_original + ' (Counts)': table[country].iloc[0]}, ignore_index=True)
+
+    return new_df
